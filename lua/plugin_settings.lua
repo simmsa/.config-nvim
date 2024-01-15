e7ae4836cbc6d81a540c34bb4331670adcd63491
Andrew Simms <simms.andrew@gmail.com>
2024-01-15 13:52:23 -0700

    Plugin: Add nvim-cmp settings

--- lua/plugin_settings.lua
+++ lua/plugin_settings.lua
@@ -429,3 +429,75 @@ vim.g.rainbow_delimiters = {
 }
 
 --  End hphish/rainbow-delimiters.nvim ----------------------------------}}}
+--  nvim-cmp Autocomplete -----------------------------------------------{{{
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+-- From: https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings for vim-vsnip
+local has_words_before = function()
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
+-- nvim-cmp setup
+local cmp = require 'cmp'
+cmp.setup {
+    snippet = {
+        expand = function(args)
+            -- vim.fn['UtliSnips#Anon'](args.body)
+            vim.fn['vsnip#anonymous'](args.body)
+        end,
+    },
+    completion = {
+        keyword_length = 1,
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),  -- Down
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+        },
+        -- From: https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings for vim-vsnip
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif vim.fn["vsnip#available"](1) == 1 then
+                feedkey("<Plug>(vsnip-expand-or-jump)", "")
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+            end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function()
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+                feedkey("<Plug>(vsnip-jump-prev)", "")
+            end
+        end, { "i", "s" }),
+    }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'vsnip' },
+        { name = 'buffer' },
+        { name = 'otter' },
+        {
+            name = 'path',
+            option = {
+                trailing_slash = true,
+                label_trailing_slash = true,
+            }
+        },
+    })
+}
+
+vim.g.vsnip_snippet_dir = '~/.config/nvim/snippets'
+
+--  End nvim-cmp Autocomplete -------------------------------------------}}}
