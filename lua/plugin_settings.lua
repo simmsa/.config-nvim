ecbe41ef5519fcf7a001af0cad7bff7eac421032
Andrew Simms <simms.andrew@gmail.com>
2024-01-17 19:31:43 -0700

    Plugin: Remove alternate file indicator from tabline

--- lua/plugin_settings.lua
+++ lua/plugin_settings.lua
@@ -123,7 +123,7 @@ require("lualine").setup({
 
 				symbols = {
 					modified = " ●", -- Text to show when the buffer is modified
-					alternate_file = "#", -- Text to show to identify the alternate file
+					alternate_file = "", -- Text to show to identify the alternate file
 					directory = "", -- Text to show when the buffer is a directory
 				},
 				-- show_filename_only = true,
