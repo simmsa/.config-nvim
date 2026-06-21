14eb0d445051b73cc847c633de4e84cf9ae7ec67
Andrew Simms <simms.andrew@gmail.com>
2026-06-21 08:33:15 -0600

    Plugin: Capture current lualine configuration

--- lua/plugin_settings.lua
+++ lua/plugin_settings.lua
@@ -28,131 +28,142 @@ vim.g.ale_linters_ignore = {
 --  lualine.nvim --------------------------------------------------------{{{
 
 local function display_file_progress()
-	return string.format("%s/%s", vim.fn.line("."), vim.fn.line("$"))
+    return string.format("%s/%s", vim.fn.line("."), vim.fn.line("$"))
+end
+
+local function display_paste()
+    -- local paste_raw = vim.api.nvim_command("echo &paste")
+    local paste_mode_active = vim.o.paste
+
+    if paste_mode_active then
+        return "PASTE"
+    else
+        return "NOPASTE"
+    end
 end
 
 require("lualine").setup({
-	options = {
-		icons_enabled = true,
-		theme = "auto",
-		-- component_separators = { left = '', right = ''},
-		-- section_separators = { left = '', right = ''},
-		disabled_filetypes = {
-			statusline = {},
-			winbar = {},
-		},
-		ignore_focus = {},
-		always_divide_middle = true,
-		globalstatus = false,
-		refresh = {
-			statusline = 1000,
-			tabline = 1000,
-			winbar = 1000,
-		},
-	},
-	sections = {
-		lualine_a = { "mode", "searchcount" },
-		-- lualine_b = {'branch', 'diff', 'diagnostics'},
-		lualine_b = { "branch", "diff" },
-		lualine_c = { "filename" },
-		-- lualine_x = {'encoding', 'fileformat', 'filetype'},
-		lualine_x = {
-			{
-				"diagnostics",
+    options = {
+        icons_enabled = true,
+        theme = "auto",
+        -- component_separators = { left = '', right = ''},
+        -- section_separators = { left = '', right = ''},
+        disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = false,
+        refresh = {
+            statusline = 1000,
+            tabline = 1000,
+            winbar = 1000,
+        },
+    },
+    sections = {
+        lualine_a = { "mode", "searchcount", display_paste },
+        -- lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_b = { "branch", "diff" },
+        lualine_c = { "filename" },
+        -- lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_x = {
+            {
+                "diagnostics",
 
-				-- Table of diagnostic sources, available sources are:
-				--   'nvim_lsp', 'nvim_diagnostic', 'nvim_workspace_diagnostic', 'coc', 'ale', 'vim_lsp'.
-				-- or a function that returns a table as such:
-				--   { error=error_cnt, warn=warn_cnt, info=info_cnt, hint=hint_cnt }
-				sources = { "nvim_lsp", "nvim_diagnostic", "ale" },
+                -- Table of diagnostic sources, available sources are:
+                --   'nvim_lsp', 'nvim_diagnostic', 'nvim_workspace_diagnostic', 'coc', 'ale', 'vim_lsp'.
+                -- or a function that returns a table as such:
+                --   { error=error_cnt, warn=warn_cnt, info=info_cnt, hint=hint_cnt }
+                sources = { "nvim_lsp", "nvim_diagnostic", "ale" },
 
-				-- Displays diagnostics for the defined severity types
-				-- sections = { 'error', 'warn', 'info', 'hint' },
+                -- Displays diagnostics for the defined severity types
+                -- sections = { 'error', 'warn', 'info', 'hint' },
 
-				-- diagnostics_color = {
-				--   -- Same values as the general color option can be used here.
-				--   error = 'DiagnosticError', -- Changes diagnostics' error color.
-				--   warn  = 'DiagnosticWarn',  -- Changes diagnostics' warn color.
-				--   info  = 'DiagnosticInfo',  -- Changes diagnostics' info color.
-				--   hint  = 'DiagnosticHint',  -- Changes diagnostics' hint color.
-				-- },
-				-- symbols = {error = 'E', warn = 'W', info = 'I', hint = 'H'},
-				-- colored = true,           -- Displays diagnostics status in color if set to true.
-				-- update_in_insert = false, -- Update diagnostics in insert mode.
-				-- always_visible = true,   -- Show diagnostics even if there are none.
-			},
-		},
-		-- lualine_y = {'progress'},
-		lualine_y = { { "filetype", icons_enabled = false } },
-		-- lualine_z = {'location'}
-		-- Disable bold text
-		lualine_z = {
-			{ display_file_progress, color = { gui = "" } },
-		},
-	},
-	inactive_sections = {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = { "filename" },
-		lualine_x = { "location" },
-		lualine_y = {},
-		lualine_z = {},
-	},
-	tabline = {
-		lualine_a = {
-			{
-				"buffers",
-				show_filename_only = true, -- Shows shortened relative path when set to false.
-				hide_filename_extension = false, -- Hide filename extension when set to true.
-				show_modified_status = true, -- Shows indicator when the buffer is modified.
+                -- diagnostics_color = {
+                --   -- Same values as the general color option can be used here.
+                --   error = 'DiagnosticError', -- Changes diagnostics' error color.
+                --   warn  = 'DiagnosticWarn',  -- Changes diagnostics' warn color.
+                --   info  = 'DiagnosticInfo',  -- Changes diagnostics' info color.
+                --   hint  = 'DiagnosticHint',  -- Changes diagnostics' hint color.
+                -- },
+                -- symbols = {error = 'E', warn = 'W', info = 'I', hint = 'H'},
+                -- colored = true,           -- Displays diagnostics status in color if set to true.
+                -- update_in_insert = false, -- Update diagnostics in insert mode.
+                -- always_visible = true,   -- Show diagnostics even if there are none.
+            },
+        },
+        -- lualine_y = {'progress'},
+        lualine_y = { { "filetype", icons_enabled = false } },
+        -- lualine_z = {'location'}
+        -- Disable bold text
+        lualine_z = {
+            { display_file_progress, color = { gui = "" } },
+        },
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
+    },
+    tabline = {
+        lualine_a = {
+            {
+                "buffers",
+                show_filename_only = true,       -- Shows shortened relative path when set to false.
+                hide_filename_extension = false, -- Hide filename extension when set to true.
+                show_modified_status = true,     -- Shows indicator when the buffer is modified.
 
-				mode = 2, -- 0: Shows buffer name
-				-- 1: Shows buffer index
-				-- 2: Shows buffer name + buffer index
-				-- 3: Shows buffer number
-				-- 4: Shows buffer name + buffer number
+                mode = 2,                        -- 0: Shows buffer name
+                -- 1: Shows buffer index
+                -- 2: Shows buffer name + buffer index
+                -- 3: Shows buffer number
+                -- 4: Shows buffer name + buffer number
 
-				max_length = vim.o.columns * 2 / 3, -- Maximum width of buffers component,
-				-- it can also be a function that returns
-				-- the value of `max_length` dynamically.
-				filetype_names = {
-					TelescopePrompt = "Telescope",
-					dashboard = "Dashboard",
-					packer = "Packer",
-					fzf = "FZF",
-					alpha = "Alpha",
-				}, -- Shows specific buffer name for that filetype ( { `filetype` = `buffer_name`, ... } )
+                max_length = vim.o.columns * 2 / 3, -- Maximum width of buffers component,
+                -- it can also be a function that returns
+                -- the value of `max_length` dynamically.
+                filetype_names = {
+                    TelescopePrompt = "Telescope",
+                    dashboard = "Dashboard",
+                    packer = "Packer",
+                    fzf = "FZF",
+                    alpha = "Alpha",
+                }, -- Shows specific buffer name for that filetype ( { `filetype` = `buffer_name`, ... } )
 
-				-- Automatically updates active buffer color to match color of other components (will be overidden if buffers_color is set)
-				use_mode_colors = false,
+                -- Automatically updates active buffer color to match color of other components (will be overidden if buffers_color is set)
+                use_mode_colors = false,
 
-				buffers_color = {
-					-- Same values as the general color option can be used here.
-					active = "lualine_a_normal", -- Color for active buffer.
-					inactive = "lualine_b_normal", -- Color for inactive buffer.
-				},
+                buffers_color = {
+                    -- Same values as the general color option can be used here.
+                    active = "lualine_a_normal",   -- Color for active buffer.
+                    inactive = "lualine_b_normal", -- Color for inactive buffer.
+                },
 
-				symbols = {
-					modified = " ●", -- Text to show when the buffer is modified
-					alternate_file = "", -- Text to show to identify the alternate file
-					directory = "", -- Text to show when the buffer is a directory
-				},
-				-- show_filename_only = true,
-				-- hide_filename_extension = true,
-				-- show_modified_status = true,
-				-- buffers_color = { active = "lualine_a_normal", inactive = "lualine_b_normal" },
-				-- buffers_color = { active = "lualine_normal_a", inactive = "lualine_inactive_a" },
-			},
-		},
-		lualine_b = {},
-		lualine_c = {},
-		lualine_x = {},
-		lualine_y = {},
-		lualine_z = {},
-	},
-	winbar = {},
-	inactive_winbar = {},
-	extensions = {},
+                symbols = {
+                    modified = " ●", -- Text to show when the buffer is modified
+                    alternate_file = "", -- Text to show to identify the alternate file
+                    directory = "", -- Text to show when the buffer is a directory
+                },
+                -- show_filename_only = true,
+                -- hide_filename_extension = true,
+                -- show_modified_status = true,
+                -- buffers_color = { active = "lualine_a_normal", inactive = "lualine_b_normal" },
+                -- buffers_color = { active = "lualine_normal_a", inactive = "lualine_inactive_a" },
+            },
+        },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
+    },
+    winbar = {},
+    inactive_winbar = {},
+    extensions = {},
 })
 
 --  End lualine.nvim ----------------------------------------------------}}}
