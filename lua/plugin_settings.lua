ddc5de030420c5f734923582aa407d6236249080
Andrew Simms <simms.andrew@gmail.com>
2026-06-21 08:39:09 -0600

    Plugin: Capture otter.nvim configuration

--- lua/plugin_settings.lua
+++ lua/plugin_settings.lua
@@ -540,33 +540,33 @@ require("oil").setup({
 --  End Oil -------------------------------------------------------------}}}
 --  otter.nvim ----------------------------------------------------------{{{
 
-local otter = require("otter")
-otter.setup({
-	lsp = {
-		hover = {
-			border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-		},
-	},
-	buffers = {
-		-- if set to true, the filetype of the otterbuffers will be set.
-		-- otherwise only the autocommand of lspconfig that attaches
-		-- the language server will be executed without setting the filetype
-		set_filetype = false,
-		-- write <path>.otter.<embedded language extension> files
-		-- to disk on save of main buffer.
-		-- usefule for some linters that require actual files
-		-- otter files are deleted on quit or main buffer close
-		write_to_disk = true,
-	},
-	strip_wrapping_quote_characters = { "'", '"', "`" },
-})
+-- local otter = require("otter")
+-- otter.setup({
+-- 	lsp = {
+-- 		hover = {
+-- 			border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+-- 		},
+-- 	},
+-- 	buffers = {
+-- 		-- if set to true, the filetype of the otterbuffers will be set.
+-- 		-- otherwise only the autocommand of lspconfig that attaches
+-- 		-- the language server will be executed without setting the filetype
+-- 		set_filetype = false,
+-- 		-- write <path>.otter.<embedded language extension> files
+-- 		-- to disk on save of main buffer.
+-- 		-- usefule for some linters that require actual files
+-- 		-- otter files are deleted on quit or main buffer close
+-- 		write_to_disk = true,
+-- 	},
+-- 	strip_wrapping_quote_characters = { "'", '"', "`" },
+-- })
 
-local otter_languages = { "python", "r" }
-local otter_completion = true
-local otter_diagnostics = true
-local otter_use_treesitter = true
+-- local otter_languages = { "python", "r" }
+-- local otter_completion = true
+-- local otter_diagnostics = true
+-- local otter_use_treesitter = true
 
-otter.activate(otter_languages, otter_completion, otter_diagnostics, otter_use_treesitter)
+-- otter.activate(otter_languages, otter_completion, otter_diagnostics, otter_use_treesitter)
 
 --  End otter.nvim ------------------------------------------------------}}}
 --  nvim-treesitter -----------------------------------------------------{{{
