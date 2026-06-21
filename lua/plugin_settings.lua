3fff687f327dc822df6ddf220b5c12a0f0dd542d
Andrew Simms <simms.andrew@gmail.com>
2026-06-21 08:36:08 -0600

    Plugins: Capture current quarto configuration

--- lua/plugin_settings.lua
+++ lua/plugin_settings.lua
@@ -266,35 +266,95 @@ require("gitsigns").setup({
 --  End gitsigns.nvim ---------------------------------------------------}}}
 --  quarto-nvim ---------------------------------------------------------{{{
 
-require("quarto").setup({
-	debug = false,
-	closePreviewOnExit = true,
-	lspFeatures = {
-		enabled = true,
-		languages = { "r", "python", "bash" },
-		chunks = "curly", -- 'curly' or 'all'
-		diagnostics = {
-			enabled = true,
-			triggers = { "BufWritePost" },
-		},
-		completion = {
-			enabled = true,
-		},
-	},
-	codeRunner = {
-		enabled = false,
-		default_method = nil, -- 'molten' or 'slime'
-		ft_runners = {}, -- filetype to runner, ie. `{ python = "molten" }`.
-		-- Takes precedence over `default_method`
-		never_run = { "yaml" }, -- filetypes which are never sent to a code runner
-	},
-	keymap = {
-		hover = "M",
-		definition = "gd",
-		-- rename = '<leader>lR',
-		references = "gr",
-	},
-})
+-- require("quarto").setup({
+-- 	debug = false,
+-- 	closePreviewOnExit = true,
+-- 	lspFeatures = {
+-- 		enabled = true,
+-- 		languages = { "r", "python", "bash" },
+-- 		chunks = "curly", -- 'curly' or 'all'
+-- 		diagnostics = {
+-- 			enabled = true,
+-- 			triggers = { "BufWritePost" },
+-- 		},
+-- 		completion = {
+-- 			enabled = true,
+-- 		},
+-- 	},
+-- 	codeRunner = {
+-- 		enabled = false,
+-- 		default_method = nil, -- 'molten' or 'slime'
+-- 		ft_runners = {}, -- filetype to runner, ie. `{ python = "molten" }`.
+-- 		-- Takes precedence over `default_method`
+-- 		never_run = { "yaml" }, -- filetypes which are never sent to a code runner
+-- 	},
+-- 	keymap = {
+-- 		hover = "M",
+-- 		definition = "gd",
+-- 		-- rename = '<leader>lR',
+-- 		references = "gr",
+-- 	},
+-- })
+
+-- require("quarto").setup({
+-- 	"quarto-dev/quarto-nvim",
+-- 	dev = false,
+-- 	debug = false,
+-- 	closePreviewOnExit = true,
+-- 	lspFeatures = {
+-- 		enabled = true,
+-- 		languages = { "r", "python", "bash" },
+-- 		chunks = "curly", -- 'curly' or 'all'
+-- 		diagnostics = {
+-- 			enabled = true,
+-- 			triggers = { "BufWritePost" },
+-- 		},
+-- 		completion = {
+-- 			enabled = true,
+-- 		},
+-- 	},
+-- 	codeRunner = {
+-- 		enabled = false,
+-- 		default_method = nil, -- 'molten' or 'slime'
+-- 		ft_runners = {}, -- filetype to runner, ie. `{ python = "molten" }`.
+-- 		-- Takes precedence over `default_method`
+-- 		never_run = { "yaml" }, -- filetypes which are never sent to a code runner
+-- 	},
+-- 	keymap = {
+-- 		hover = "M",
+-- 		definition = "gd",
+-- 		-- rename = '<leader>lR',
+-- 		references = "gr",
+-- 	},
+-- 	dependencies = {
+-- 		{
+-- 			"jmbuhr/otter.nvim",
+-- 			dev = false,
+-- 			dependencies = {
+-- 				{ "neovim/nvim-lspconfig" },
+-- 			},
+-- 			opts = {
+-- 				lsp = {
+-- 					hover = {
+-- 						border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+-- 					},
+-- 				},
+-- 				buffers = {
+-- 					-- if set to true, the filetype of the otterbuffers will be set.
+-- 					-- otherwise only the autocommand of lspconfig that attaches
+-- 					-- the language server will be executed without setting the filetype
+-- 					set_filetype = true,
+-- 					write_to_disk = true,
+-- 				},
+-- 			},
+-- 		},
+-- 	},
+-- 	opts = {
+-- 		lspFeatures = {
+-- 			languages = { "r", "python", "bash", "html", "dot" },
+-- 		},
+-- 	},
+-- })
 
 --  End quarto-nvim -----------------------------------------------------}}}
 --  otter.nvim ----------------------------------------------------------{{{
